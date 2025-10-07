ed52418b5df41e980573b4f75875e82619d34f49
Daniel Tsegay <danieltsegay7939@gmail.com>
2025-10-07 12:34:41 +0300

    deploying on netlify

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
  const axiosInstance = axios.create({
-    baseURL: "http://127.0.0.1:5001/clone-70221/us-central1/api",
+    // baseURL: "http://127.0.0.1:5001/clone-70221/us-central1/api",
+    baseURL: "https://amazon-api-deploy-2025-crdg.onrender.com",
 });
     export default axiosInstance;
